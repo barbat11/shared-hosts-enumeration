d881b560f0e6f6265da76f729103179963108a2a
Paul Barbat <paul.barbat93@gmail.com>
2019-02-14 15:43:26 +0200

    Create shared-enum.sh

---
+++ shared-enum.sh
@@ -0,0 +1,20 @@
+#Shared Hosting Enumeration script by Paul Barbat
+#Usage: ./script.sh ip-list.txt
+#Each IP output in separate directory
+
+self="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+exists="$(type aquatone)"
+
+if [[ ${exists} == *"not found"* ]]; then
+  printf "Error: Aquatone not installed. To install, visit https://github.com/michenriksen/aquatone."
+  exit 0
+fi
+
+if [ $# -lt 1 ]; 
+   then 
+   printf "Error: not enough arguments. Usage: ./${self} <list-of-IPs>\n" $# 
+   exit 0 
+   fi 
+
+while read LINE; do curl https://api.hackertarget.com/reverseiplookup/?q=$LINE | aquatone -scan-timeout 500 -out $(echo $LINE | tr "." "-"); done < $1
+#echo ${OUTPUT} | tr " " "\n" | aquatone -scan-timeout 500 -out $2
